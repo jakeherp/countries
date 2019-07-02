23e57258801c1715ad8bfcaabebc862a0e4b20b6
Jacob Herper <jacob@herper.uk>
2019-07-02 11:35:44 +0100

    API implemented and filter created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react"
+import ReactDOM from "react-dom"
+import axios from "axios"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+	const [countries, setCountries] = useState([])
+	const [filter, setFilter] = useState("")
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	useEffect(() => {
+		const fetchCountries = async () => {
+			const data = await axios.get("https://restcountries.eu/rest/v2/all")
+			setCountries(data.data)
+		}
+		fetchCountries()
+	}, [])
+
+	const handleFilter = e => setFilter(e.target.value)
+
+	const filteredCountries = !filter
+		? countries
+		: countries.filter(country =>
+				country.name.toLowerCase().includes(filter.toLowerCase()),
+		  )
+
+	return (
+		<React.Fragment>
+			<form onSubmit={e => e.preventDefault()}>
+				<div>
+					<input
+						value={filter}
+						placeholder="Filter countries"
+						onChange={handleFilter}
+					/>
+				</div>
+			</form>
+			<ul>
+				{filter && filteredCountries.length >= 10
+					? "Too many matches, please be more precise"
+					: filteredCountries.map(country => (
+							<li key={country.alpha2Code}>
+								<h2>
+									<img
+										src={country.flag}
+										style={{ height: `1em` }}
+										alt={country.name}
+									/>{" "}
+									{country.name}
+								</h2>
+								<ul>
+									<li>Capital: {country.capital}</li>
+									<li>
+										Population:{" "}
+										{country.population.toLocaleString(
+											navigator.language,
+											0,
+										)}
+									</li>
+									<li>
+										Official languages:
+										<ul>
+											{country.languages.map(lang => (
+												<li key={lang.iso639_1}>
+													{lang.name}
+												</li>
+											))}
+										</ul>
+									</li>
+								</ul>
+							</li>
+					  ))}
+			</ul>
+		</React.Fragment>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
